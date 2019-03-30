02e2f65fcf9644d3df35da91f0b7731e82517227
Jordan Koelbl <jkoelbl@csumb.edu>
2019-03-30 12:38:21 -0700

    removed db_helper, reworked simple_sqlite3 for more generalized purposes, renamed simple_sqlite3 to simple_db

--- db_helper.py
+++
@@ -1,5 +0,0 @@
-import sqlite3
-
-def make_mapped(descriptor, response_list):
-    headers = [d[0] for d in descriptor]
-    return [{headers[i]:response[i] for i in range(len(response))} for response in response_list]
